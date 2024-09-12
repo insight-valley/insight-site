23b6755420a6ce79881e9f57bf9e42288eaf2ec9
gabriel-dantas98 <gabriel.dantas@quintoandar.com.br>
2024-09-12 11:28:52 -0300

    CNAME e social links

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -77,24 +77,9 @@ export default function Footer() {
           <div className="md:flex md:items-center md:justify-between">
             {/* Social links */}
             <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
-              <li>
-                <Link
-                  href="/"
-                  className="flex items-center justify-center transition duration-150 ease-in-out bg-gray-800 rounded-full text-accent-100 hover:text-gray-100 hover:bg-pink-500"
-                  aria-label="Twitter"
-                >
-                  <svg
-                    className="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
-                  </svg>
-                </Link>
-              </li>
               <li className="ml-4">
                 <Link
-                  href="/"
+                  href="https://github.com/insight-valley"
                   className="flex items-center justify-center transition duration-150 ease-in-out bg-gray-800 rounded-full text-accent-100 hover:text-gray-100 hover:bg-pink-500"
                   aria-label="Github"
                 >
@@ -109,22 +94,7 @@ export default function Footer() {
               </li>
               <li className="ml-4">
                 <Link
-                  href="/"
-                  className="flex items-center justify-center transition duration-150 ease-in-out bg-gray-800 rounded-full text-accent-100 hover:text-gray-100 hover:bg-pink-500"
-                  aria-label="Facebook"
-                >
-                  <svg
-                    className="w-8 h-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
-                  </svg>
-                </Link>
-              </li>
-              <li className="ml-4">
-                <Link
-                  href="/"
+                  href="https://www.instagram.com/insight.valley/"
                   className="flex items-center justify-center transition duration-150 ease-in-out bg-gray-800 rounded-full text-accent-100 hover:text-gray-100 hover:bg-pink-500"
                   aria-label="Instagram"
                 >
@@ -141,7 +111,7 @@ export default function Footer() {
               </li>
               <li className="ml-4">
                 <Link
-                  href="/"
+                  href="https://www.linkedin.com/company/insight-valley"
                   className="flex items-center justify-center transition duration-150 ease-in-out bg-gray-800 rounded-full text-accent-100 hover:text-gray-100 hover:bg-pink-500"
                   aria-label="Linkedin"
                 >
